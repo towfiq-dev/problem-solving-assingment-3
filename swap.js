08817237cd75eb826651368b9a04c817b66f2d18
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-02-04 11:05:30 +0600

    added something

---
+++ swap.js
@@ -0,0 +1,18 @@
+let a = 5
+let b = 7
+let temp = a
+a = b
+b = temp
+console.log(a,b)
+
+let towfiq = 'j'
+let saiful = "K"
+let temps =towfiq
+towfiq =saiful
+saiful =temps
+console.log(towfiq, saiful)
+let x = 5;
+let y = 7;
+
+[x, y] = [y, x];
+console.log(x, y)
